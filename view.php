a6c37c59f3fb206bc530b6391023c073e3deb7ee
Sai Chandana Megavath Bapuji <70906163+saichandana23@users.noreply.github.com>
2022-09-08 07:21:31 +0530

    Add files via upload
    assignment 1

---
+++ view.php
@@ -0,0 +1,30 @@
+<html>
+<link rel="stylesheet" href="view.css">
+<head>
+    <title>GYM MANAGEMENT SYSTEM</title>
+</head>
+<body>
+<div id="hd">GYM MANAGEMENT SYSTEM</div>
+<main>
+    <form name="REGISTRATION" action="" class="form-content">
+        <label>NAME:</label>
+        <input type="text" name="name"/><br><br>
+        <label>EMAIL:</label>
+        <input type="text" name="email"/><br><br>
+        <label>PHONE NUMBER:</label>
+        <input type="text" name="Mobile Number"><br><br>
+        <label>GENDER:</label>
+        <input type="text" name="GENDER"><br><br>
+        <label>WORKOUT TYPE:</label>
+        <select name="workout" >
+            <option value="">CHOOSE ONE</option>
+            <option value="AEROBICS">AEROBICS</option>
+            <option value="ZUMBA">ZUMBA</option>
+            <option value="CARDIO">CARDIO</option>
+            <option value="SPORTS">SPORTS</option>
+            <option value="STRETCHING">STRETCHING</option>
+        </select>
+    </form>
+</main>
+</body>
+</html>
